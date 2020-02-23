85014538d6cb754488049fb73563c3e7cae13bea
Webb-balson <balsongorai@gmail.com>
2020-02-23 16:52:49 +0530

    Completed day 2 challenge

---
+++ day_2/variables.py
@@ -0,0 +1,40 @@
+# Day 2: 30 Days of python programming
+first_name = 'Balson'
+last_name = 'Gorai'
+full_name = "Balson Gorai"
+country = 'India'
+city = 'Bangalore'
+age = 23
+year = 2020
+is_married = False
+is_true = True
+is_light_on = True
+a,b,c = 1,2,3
+print()
+print(type(first_name))
+print(type(age))
+print(type(is_married))
+print(type(is_light_on))
+print(type(a))
+print()
+print(len(first_name))
+print()
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one/num_two
+remainder = num_one%num_two
+exp = num_one**num_two
+floor_division = num_one//num_two
+radius = 30
+area_of_circle = 3.14*radius*radius
+circum_of_circle = 2*3.14*radius
+print(area_of_circle)
+radius = int(input('What is the radius of circle?'))
+area_of_circle = 3.14*radius*radius
+print('Full Name:',full_name)
+print(age)
+print(area_of_circle)
+help('keywords')
